665f5533a242dc76c00916980b791fe6ca6eab03
rohnins <denissrohnin@gmail.com>
2023-09-14 17:48:50 +0200

    changed the tests

--- tests/test_ultrasensor.py
+++ tests/test_ultrasensor.py
@@ -2,9 +2,9 @@ import unittest.mock as mock
 import pytest
 import ultrasensor
 
-def test_update():
+def test_read_distance():
     """
-    Test the update method of the SoundObserver class
+    Test the read_distance function from ultrasensor
     """
     with mock.patch('ultrasensor.RPi.GPIO') as mock_gpio:
         # Set up the mock
@@ -16,24 +16,3 @@ def test_update():
 
         # Assert the expected behavior
         assert distance == pytest.approx(0.343, rel=1e-3)
-        
-    with mock.patch('ultrasensor.pygame.mixer.music') as mock_music:
-        # Create a SoundObserver instance
-        observer = ultrasensor.SoundObserver()
-
-        # Call the update method with a distance less than the threshold
-        observer.update(ultrasensor.THRESHOLD_DISTANCE - 1)
-
-        # Assert that a sound was played
-        mock_music.load.assert_called_once()
-        mock_music.play.assert_called_once()
-
-        # Reset the mock
-        mock_music.reset_mock()
-
-        # Call the update method with a distance greater than the threshold
-        observer.update(ultrasensor.THRESHOLD_DISTANCE + 1)
-
-        # Assert that no sound was played
-        mock_music.load.assert_not_called()
-        mock_music.play.assert_not_called()
