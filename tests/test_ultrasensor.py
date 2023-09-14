a7c73d620cf6aeade1be7e2e88bc3706e6b25940
rohnins <denissrohnin@gmail.com>
2023-09-14 17:44:13 +0200

    added another mock to RPi.GPIO

--- tests/test_ultrasensor.py
+++ tests/test_ultrasensor.py
@@ -6,6 +6,17 @@ def test_update():
     """
     Test the update method of the SoundObserver class
     """
+    with mock.patch('ultrasensor.RPi.GPIO') as mock_gpio:
+        # Set up the mock
+        mock_gpio.input.side_effect = [0, 1, 0]
+        mock_gpio.time.time.side_effect = [0, 0.00001, 0.00002]
+
+        # Call the function
+        distance = ultrasensor.read_distance()
+
+        # Assert the expected behavior
+        assert distance == pytest.approx(0.343, rel=1e-3)
+        
     with mock.patch('ultrasensor.pygame.mixer.music') as mock_music:
         # Create a SoundObserver instance
         observer = ultrasensor.SoundObserver()
