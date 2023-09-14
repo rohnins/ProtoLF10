9cf525829979c818639bc4da8abdebfced248304
rohnins <denissrohnin@gmail.com>
2023-09-14 21:52:05 +0200

    magic numbers commited

--- ultrasensor.py
+++ ultrasensor.py
@@ -4,21 +4,21 @@ import random
 import os
 import pygame
 import pytest
-#blab
+
 # Constants
 PIN_TRIGGER = 7
 PIN_ECHO = 11
-# for the distance, everything below the number triggers a sound to happen. Make sure to adjust this number. I selected 10cm for testing.
-THRESHOLD_DISTANCE = 10.33 # Our determined distance for idle, any change in that distance gives a reaction
-SOUNDS_DIR = "/home/admin/Music" # The path on our rasp, where all MP3 files are located, we just select the music folder.
+THRESHOLD_DISTANCE_CM = 10.33
+MAX_TIMEOUT = 1.0
+SOUNDS_DIR = "/home/admin/Music"
 
-# initialize GPIO
+# GPIO setup
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIN_TRIGGER, GPIO.OUT)
 GPIO.setup(PIN_ECHO, GPIO.IN)
 pygame.mixer.init()
 
-# designpattern observer
+# Observer pattern
 class Subject:
     def __init__(self):
         self.observers = []
@@ -38,7 +38,7 @@ class SoundObserver:
         self.sound_files = os.listdir(SOUNDS_DIR)
 
     def update(self, distance):
-        if distance < THRESHOLD_DISTANCE:
+        if distance < THRESHOLD_DISTANCE_CM:
             self.play_random_sound()
 
     def play_random_sound(self):
@@ -46,11 +46,11 @@ class SoundObserver:
         sound_path = os.path.join(SOUNDS_DIR, selected_sound)
         print(f"Playing sound: {selected_sound}")
         
-        # now we use pygame to play the selected sound
+        # Use pygame to play the selected sound
         pygame.mixer.music.load(sound_path)
         pygame.mixer.music.play()
- 
- # Create instances of the Subject and SoundObserver
+
+# Create instances of the Subject and SoundObserver
 sensor = Subject()
 sound_observer = SoundObserver()
 sensor.add_observer(sound_observer)
@@ -65,12 +65,12 @@ def read_distance():
     # Wait for the echo to return
     start_time = time.time()
     while GPIO.input(PIN_ECHO) == 0:
-        if time.time() - start_time > 1.0:
+        if time.time() - start_time > MAX_TIMEOUT:
             return -1  # Timeout
 
     echo_start_time = time.time()
     while GPIO.input(PIN_ECHO) == 1:
-        if time.time() - echo_start_time > 1.0:
+        if time.time() - echo_start_time > MAX_TIMEOUT:
             return -1  # Timeout
 
     pulse_duration = time.time() - echo_start_time
@@ -83,58 +83,9 @@ def read_distance():
 try:
     while True:
         distance = read_distance()
-        print ("Distance:",distance,"cm") #this is just for testing to measure the distance. If everything is settled, just comment this out.
+        print("Distance:", distance, "cm")
         sensor.notify_observers(distance)
         time.sleep(1)
 
 except KeyboardInterrupt:
-    GPIO.cleanup()       
-
-
-
-""" #old file, for reference. if we need to measure the exact distance in our container.
-
-#!/home/admin
-#init
-import RPi.GPIO as GPIO
-import time
-import random
-import os
-import pygame
-
-
-try:
-      GPIO.setmode(GPIO.BOARD)
-
-      PIN_TRIGGER = 7
-      PIN_ECHO = 11
-
-      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-      GPIO.setup(PIN_ECHO, GPIO.IN)
-
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-      print ("Waiting for sensor to settle")
-
-      time.sleep(2)
-
-      print ("Calculating distance")
-
-      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-
-      time.sleep(0.00001)
-
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-      while GPIO.input(PIN_ECHO)==0:
-            pulse_start_time = time.time()
-      while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()
-
-      pulse_duration = pulse_end_time - pulse_start_time
-      distance = round(pulse_duration * 17150, 2)
-      print ("Distance:",distance,"cm")
-
-finally:
-      GPIO.cleanup()
-"""
+    GPIO.cleanup()
