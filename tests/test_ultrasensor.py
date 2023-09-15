7d3176c5534d2cc6fc6914625bbdc81e92eb9f51
rohnins <denissrohnin@gmail.com>
2023-09-15 09:34:36 +0200

    changed the tests again

--- tests/test_ultrasensor.py
+++ tests/test_ultrasensor.py
@@ -1,15 +1,9 @@
-import pytest
+import pytests
 import ultrasensor
 
 # Check if SOUNDS_DIR exists
 def test_sounds_dir_exists():
     assert os.path.exists(SOUNDS_DIR), "SOUNDS_DIR does not exist"
-
-# Check GPIO setup
-def test_gpio_setup():
-    assert GPIO.getmode() == GPIO.BOARD, "GPIO mode is not set to BOARD"
-    assert GPIO.gpio_function(PIN_TRIGGER) == GPIO.OUT, "PIN_TRIGGER is not set as an output pin"
-    assert GPIO.gpio_function(PIN_ECHO) == GPIO.IN, "PIN_ECHO is not set as an input pin"
 
 # Check distance calculation
 def test_distance_calculation():
